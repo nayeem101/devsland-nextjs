14896250fe7e37ea914148468f16be76cd4e48d5
nayeemf25 <inayeem044@gmail.com>
2023-08-21 22:41:25 +0600

    initial setup and navbar done

---
+++ components/Footer.tsx
@@ -0,0 +1,18 @@
+import { footer } from "@/dictionaries/en-us.dict";
+import Image from "next/image";
+
+const Footer = () => {
+  return (
+    <footer className="flexStart footer">
+      <div className="flex flex-col gap-12 w-full">
+        <div className="flex items-start flex-col">
+          <Image src="/logo-purple.svg" width={115} height={38} alt="flexible" />
+
+          <p className="text-start text-sm font-normal mt-5 max-w-xs">{footer.description}</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ dictionaries/en-us.dict.ts
@@ -0,0 +1,3 @@
+export const footer = {
+  'description': 'DevsLand is a project showcase site for developers'
+}
